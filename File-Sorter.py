3f3047775452697643d00ac63638624434a936c0
Madhu Boyapalli <166612543+MadhuBoyapalli@users.noreply.github.com>
2025-01-10 10:55:37 +0530

    Update File-Sorter.py

--- File-Sorter.py
+++ File-Sorter.py
@@ -2,17 +2,28 @@ import os, shutil
 
 path=r"C:/Users/HP1/Documents/"
 
+#  using listdir we get all the files present in the ''path''.
+
 complete_files=os.listdir(path)
 
 #  check for folders if exists are not.
+
 print(os.path.exists(path+'txt files'))
 
+# create a list of extension that are required.
+
 folder_names=['csv files','text files','pdf files','excel files','image files','video files']
+
+# create individual folders based on extension using ''folder_names'' if not exist.
 
 for folders in folder_names:
     if not  os.path.exists(path+folders):
        os.makedirs(path+folders)
        print('created')
+        
+# after creation of folders.
+
+# filter files based on their extension.
 
 for file in complete_files:
 
